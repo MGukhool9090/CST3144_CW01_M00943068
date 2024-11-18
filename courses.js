e919f1c533a1562e6e30bdcf6ec54990062b9bb3
MGukhool9090 <mg1415@live.mdx.ac.uk>
2024-11-18 10:07:13 +0400

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,102 @@
+
+let courses = [
+    {
+        id:1001,
+        title:"Mathematics",
+        description:"A level mathematics class",
+        price: 50.00,
+        image: "images/maths.png",
+        availableInventory: 20,
+        rating:4
+    },
+
+    {
+        id:1002,
+        title:"French",
+        description:"A level french class",
+        price: 100.00,
+        image: "images/french.png",
+        availableInventory: 15,
+        rating:3
+    },
+
+    {
+        id:1003,
+        title:"Art",
+        description:"A level art class",
+        price: 55.00,
+        image: "images/art.png",
+        availableInventory: 21,
+        rating:2
+    },
+
+    {
+        id:1004,
+        title:"English",
+        description:"A level mathematics class",
+        price: 75.00,
+        image: "images/english.png",
+        availableInventory: 16,
+        rating:5
+    },
+
+    {
+        id:1005,
+        title:"Hindi",
+        description:"A level hindi class",
+        price: 35.00,
+        image: "images/hindi.png",
+        availableInventory: 9,
+        rating:2
+    },
+
+    {
+        id:1006,
+        title:"Biology",
+        description:"A level biology class",
+        price: 69.00,
+        image: "images/biology.png",
+        availableInventory: 18,
+        rating:4
+    },
+
+    {
+        id:1007,
+        title:"Physics",
+        description:"A level physics class",
+        price: 99.00,
+        image: "images/physics.png",
+        availableInventory: 10,
+        rating:5
+    },
+
+    {
+        id:1008,
+        title:"Chemistry",
+        description:"A level chemistry class",
+        price: 89.00,
+        image: "images/chemistry.png",
+        availableInventory: 7,
+        rating:1
+    },
+
+    {
+        id:1009,
+        title:"Computer Science",
+        description:"A level computer science class",
+        price: 79.00,
+        image: "images/computerscience.png",
+        availableInventory: 20,
+        rating:3
+    },
+
+    {
+        id:1010,
+        title:"Social Studies",
+        description:"A level social studies class",
+        price: 49.00,
+        image: "images/socialstudies.png",
+        availableInventory: 11,
+        rating:2
+    },
+]
